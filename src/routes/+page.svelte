4eceb5a3111dbf39ea35f93a9f6b55a8e484b689
priyaan99 <priyaanyadav@gmail.com>
2022-12-15 18:21:41 +0530

    impl working todo app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,103 @@
-<script> 
-    let quantity = 0;
+<script>
+    import 'milligram';
 
-    function add_to_cart() {
-        quantity += 1;
+    let todos = [
+        {title: "First Todo", is_done: false},
+        {title: "Second Todo", is_done: false},
+    ];
 
-        inventory = [...inventory, quantity];
+    let num_of_todos = todos.length;
+
+    let todo_name = '';
+    let todo_done = false;
+
+    function add_todo() {
+        if (todo_name.length == 0) return; 
+
+        let new_todo = {title: todo_name, is_done: todo_done};
+        todos = [...todos, new_todo];
+
+        todo_name = '';
+        todo_done = false;
     }
 
-    let inventory = [1, 2, 3];
-    let price = 100;
+    function delete_todo(index) {
+        let todo = todos[index];
+        todos = todos.filter((t) => t != todo);
+    }
 
-    $: remaining = 10 - quantity;
-    $: total_cost = price * quantity;
 </script>
 
-<p>Count {quantity}</p>
-<button on:click={add_to_cart}>Add to Cart</button>
+<body>
 
-<p>In inventory {inventory}</p>
-<p>Remaining: {remaining}</p>
-<h3>Total cost: {total_cost}</h3>
+<div class="title">
+  <h2>Todo App</h2>
+</div>
+
+<table>
+  <thead>
+    <tr>
+      <th>Title</th>
+      <th>Progress</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+  {#each todos as todo, i (i)}
+    <tr>
+      <td>{todo.title}</td>
+      {#if todo.is_done}
+        <td>Done</td>
+      {:else}
+        <td>undone</td>
+      {/if}
+      <td>
+        <button on:click={() => delete_todo(i)}>Delete</button>
+      </td>
+    </tr>
+  {/each}
+  </tbody>
+</table>
+
+<div class="new_todo">
+
+<h2>New Todo</h2>
+<table>
+  <tbody>
+    <tr>
+      <td><h3>Title</h3></td>
+      <td>
+        <input type="text" bind:value={todo_name}/>
+      </td>
+    </tr>
+    <tr>
+      <td><h3>Progress</h3></td>
+      <td><input type="checkbox" bind:checked={todo_done}/></td>
+    </tr>
+  </tbody>
+</table>
+
+</div>
+
+<button on:click={() => {add_todo()}}>Add todo</button>
+
+</body>
+
 
 <style>
-    
+
+body {
+    padding: 1rem;
+}
+
+.title {
+    display: flex;    
+    align-items: center;
+    justify-content: center;
+}
+
+.new_todo {
+    padding-top: 2rem;
+}
+
 </style>
